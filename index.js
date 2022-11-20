e9b7b82a233dec65f5506860ea1ed2aa35c6cdaa
ILoveAndLikePizza <nathankoning@gmail.com>
2022-11-20 20:09:55 +0100

    More tiny updates

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { BrowserWindow, app } = require("electron");
+const { BrowserWindow, app, ipcMain } = require("electron");
 const { join } = require("path");
 
 let mainWindow;
@@ -8,21 +8,28 @@ app.on("ready", function() {
 	    width: 800,
 	    height: 600,
 	    show: false,
-    	    webPreferences: {
-		preload: join(__dirname, "src/preload.js"),
-		webviewTag: true,
-		sandbox: false,
-		contextIsolation: false
+		webPreferences: {
+			preload: join(__dirname, "src/preload.js"),
+			webviewTag: true,
+			sandbox: false,
+			contextIsolation: false
 	    }
     });
     mainWindow.setBackgroundColor("#000032");
     mainWindow.title = "Official N&M Games Browser";
     mainWindow.maximize();
-    // mainWindow.removeMenu();
+    mainWindow.removeMenu();
     mainWindow.setIcon("static/icon.png");
     mainWindow.loadFile("src/index.html");
     mainWindow.once("ready-to-show", function() {
         mainWindow.show();
     });
+	
+	mainWindow.webContents.on("did-attach-webview", (_e, contents) => {
+		contents.on("before-input-event", (e, input) => {
+			if (input.control && input.key.toLowerCase() == "t") mainWindow.webContents.send("newTab");
+		});
+	});
+	ipcMain.on("toggleDevTools", (_e) => mainWindow.webContents.toggleDevTools());
     
 });
--- src/preload.js
+++ src/preload.js
@@ -1,12 +1,28 @@
-addEventListener("DOMContentLoaded", function(_e) {
+const { ipcRenderer } = require("electron");
+
+let tabs = [{webview: null, pinned: false, muted: false}];
+
+const createTab = (url) => {
 	const input = document.querySelector("input");
 	const webview = document.createElement("webview");
-	webview.src = "https://nm-games.eu";
+	webview.src = url;
     webview.addEventListener("load-commit", function(e) {
  		input.value = e.url;
 	});
     input.addEventListener("keydown", function(e) {
 		if (e.key == "Enter") webview.src = input.value;
+	});
+};
+
+addEventListener("DOMContentLoaded", function(_e) {
+	const input = document.querySelector("input");
+	window.addEventListener("keydown", function(e) {
+		if (e.key.toLowerCase() == "e") {
+			ipcRenderer.send("toggleDevTools");
+		}
+	});
+	ipcRenderer.on("newTab", function() {
+		tabs.push({url: "nm://newtab", pinned: false, muted: false});
 	});
 	document.body.appendChild(webview);
 });
